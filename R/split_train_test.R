f4d6679e3f43cf99ab5e0854687e40f411ad56ed
AxelEutarici <114355274+AxelEutarici@users.noreply.github.com>
2022-12-04 15:17:39 +0100

    Update to new split w/o stratify

--- R/split_train_test.R
+++ R/split_train_test.R
@@ -1,23 +1,24 @@
 # Stratified Split on train/test datasets based on caret package
 
-library(caret)
+split_train_test<-function(data,p=0.67){
+  
+  #check if data is a data frame
+  if(!is.data.frame(data)){
+    stop("The data source must be a data frame")
+  }
+  
+  #check if p in ]0;1[
+  if(p>1 || p<=0){
+    stop("p must be a number between 0.0 and 1.0")
+  }
+  
+  #select the indices for individuals in the training sample
+  i_sample<-sample(1:nrow(data),trunc(nrow(data)*p))
+  
+  #Output list
+  res.split = list("df_train" = data[i_sample,],
+                   "df_test" = data[setdiff(1:nrow(data),i_sample),],
+                   "Split" = p)
 
-split_train_test = function(data, class, p){
-  train.index = createDataPartition(data[,class], p, list = FALSE, times = 1)
-  
-  train = data[train.index,]
-  test = data[-train.index,]
-  
-  # Return
-  
-  res.split = list("Train" = train,
-                   "Test" = test,
-                   "Split" = p,
-                   "Class.names"= levels(data[,class]),
-                   "Class.prop"=prop.table(table(data[,class]))
-                   
-    
-  )
+  return(res.split)
 }
-
-
