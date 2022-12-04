01f745430af9b8431c46363aad025de1f2d10229
Florian <55199063+Skarbkit@users.noreply.github.com>
2022-12-04 19:19:39 +0100

    almost final version

--- R/pls.fit.R
+++ R/pls.fit.R
@@ -6,6 +6,7 @@
 ### 
 ###   Ressources included in Methods and formulas for x- 
 ###   and y-statistics in Partial Least Squares Regression from support.minitab.com
+###   https://support.minitab.com/en-us/minitab/20/help-and-how-to/statistical-modeling/regression/how-to/partial-least-squares/methods-and-formulas/component-information/
 
 
 
@@ -23,6 +24,12 @@
 #' Default is to perform mean centering.
 #' @param reduce logical.  If \code{TRUE} descriptive variables are reduce.  
 #' Default is to reduce by SD of each variable in the data matrix.
+#' 
+#' @example 
+#' 
+#' res.pls = pls.fit(Species ~ ., data = iris, ncomp = 3)
+#' print.pls(res.pls)
+#' summary.pls(res.pls)
 
 #' @keywords regression multivariate
 #' @export
@@ -36,10 +43,9 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
   # data
   if(is.data.frame(data)==FALSE){stop("Error : input data isn't a dataframe object !")}
   
-  # A modifier avec la formule (selon les composantes choisis)
+  # Number of columns selected in the formula
   NbXcol = length(attributes(terms(formula, data = data))$term.labels)
-  #Nbcol = ncol(data)
-  
+
   # ncomp
   if(is.numeric(ncomp)==FALSE){stop("Error : input ncomp isn't a numeric object !")}
   if(as.integer(ncomp)!=ncomp){stop("Error : input ncomp isn't an integer !")}
@@ -51,7 +57,7 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
   if(!is.logical(reduce)){"Error : input reduce isn't a logical value !"}
   
   
-  # 
+  # Get colnames for X and y
   Xcolnames = attributes(terms(formula, data = data))$term.labels
   ycolname = toString(formula[[2]])
   
@@ -69,22 +75,16 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
   # Fit the model 
   nipals.res = nipals(X, ncomp, center=center, reduce=reduce)
   
-  
-  # Attention il faudrait centrer réduire X dans la fit avant le nipals pour avoir 
-  # les bons résultats (donc changer les paramètres d'entrée de la nipals et faire les 
-  # calculs dans la fit)
-  
   # Initialize x results matrix 
   x.residuals = X
   x.loadings = nipals.res$loadings
   x.scores = nipals.res$scores
   x.weights = nipals.res$contrib
   
-  # x.residuals contient la variance des dimensions prédites qui n'est pas expliqué par le 
-  # modèle pls (nipals pour nous). Les individus avec des résidus relativement large sont 
-  # des anomalies (outliers) qui indique qu'ils ne sont pas bien expliqué par le modèle.
-  
-  for (j in 1:ncomp){ # ncomp pour la matrice residuals
+  # x.residuals holds the variance of predicted dimensions which isn't included in our nipals's algoritm.
+  # Individuals with large values are outliers, they aren't well explaned within our model.
+
+  for (j in 1:ncomp){ # ncomp for x.residuals
     for (i in 1:n){
       x.residuals[i,j] = x.residuals[i,j] - x.scores[i,] %*% x.loadings[j,]
     }
@@ -93,6 +93,7 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
   # r, number of response on the class
   r = nlevels(y)
   
+  # dummification of y
   y.dm = dummies(y)
   
   # Initialize y results matrix
@@ -104,15 +105,10 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
     y.loadings[i,] = t(t(x.scores) %*% y.dm[,i])
     
   }
-  #y.scores = y.dm[,1]%*%y.loadings
-   # Prend la première colonne pour avoir un cas binaire
-  # for (i in 1:r){
-  #   y.scores[,i] = y.dm%*%y.loadings[,i]
-  # }
-   # Pas certain sur les y.scores, on a une colonne y 0/1 donc la moitié des scores est rempli de 0
   
-  #y.pred = x.scores %*% t(y.loadings) # > 0 égale à 1 donc valeur positive, la pred est ok
-  
+  # Y scores
+  y.scores = y.dm%*%y.loadings
+
 
   # Coefficients values
   df_pls <- data.frame(nipals.res$scores,y)
@@ -135,7 +131,8 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
                   "x.loadings" = x.loadings,
                   "y.loadings" = y.loadings,
                   "x.weights" = x.weights,
-                  "ynames" = colnames(y),
+                  "x.residuals" = x.residuals,
+                  "ynames" = levels(y),
                   "Xnames" = Xcolnames,
                   "N_comp" = ncomp,
                   "coefs" = coefs
@@ -160,13 +157,16 @@ pls.fit <- function(formula, data, ncomp=2, center=T, reduce=F){
 #'
 print.pls = function(PLSDA){
   res = rbind(PLSDA$intercept, PLSDA$coef)
-  y.loadings = PLSDA$y.loadings
+  Y_Loadings = PLSDA$y.loadings
+  row.names(Y_Loadings) = PLSDA$ynames
+  
+  
   
   cat("Coefficients : \n")
   print(res)
-  print("\n")
+  cat("\n")
   cat("Y Loadings : \n")
-  print(y.loadings)
+  print(Y_Loadings)
 }
 
 # Summary PLS
@@ -182,5 +182,5 @@ print.pls = function(PLSDA){
 #'
 
 summary.pls = function(PLSDA){
-  # a compléter
+
 }
